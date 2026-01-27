c26cf288fa5e7b270b8ae0eee1d1af87056b171c
Giang <giantgiang@gmail.com>
2026-01-27 10:57:07 +0700

    fix error scroll horizontal

--- src/components/FinanceTable.jsx
+++ src/components/FinanceTable.jsx
@@ -3,10 +3,10 @@ import "../styles/finance-table.css";
 function FinanceTable() {
   return (
     <div className="finance-wrapper">
-      <iframe
-        title="Quỹ Bóng 2025 CCF"
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRLvVHFhwFPzJZ5H-OV-nk1kDXTFqPzPWn19pbOkv_Y5_jNmgUyogKWTcvpmuFXOA/pubhtml?gid=1775787957&single=true&widget=false&headers=false&chrome=false"
-        className="finance-iframe"
+      <iframe 
+      title="Quỹ Bóng 2025 CCF"
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRLvVHFhwFPzJZ5H-OV-nk1kDXTFqPzPWn19pbOkv_Y5_jNmgUyogKWTcvpmuFXOA/pubhtml?gid=1775787957&amp;single=true&amp;widget=true&amp;headers=false"
+      className="finance-iframe"
       />
     </div>
   );
