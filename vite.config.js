aa6af3b4decc73ad765ff10f64766524388b72e8
Giang <giantgiang@gmail.com>
2026-03-11 18:06:02 +0700

    sửa config vite để đổi tên miền ccf.asia

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CCF/',  // 👈 đây là tên repo của bạn
+  base: './',  // 👈 đây là tên repo của bạn
   plugins: [react()],
 })
